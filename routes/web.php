5161b2590f0b986e02375e0b1f0ff8f0f6488692
Maissa Soussi <maissa.soussi1920@yahoo.com>
2020-08-20 12:27:31 +0100

    config

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('views/dashboard');
+});*/
+
+Route::get("/", "AdminHomeController@dashboard")->name('dashboard') ;
+Route::get("/liste-vehicules", "VehiculeController@listeVehicules")->name('listevehicules') ;
+Route::get("/liste-clients", "ClientController@listeClients")->name('listeclients') ;
