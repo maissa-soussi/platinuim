90cb4bbacd9054926f9bf0df1e48acf8255cc99d
Maissa Soussi <maissa.soussi1920@yahoo.com>
2020-09-01 23:55:13 +0100

    logo-leftbar-side

--- resources/views/layouts/left_sidebar.blade.php
+++ resources/views/layouts/left_sidebar.blade.php
@@ -2,7 +2,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4 bg-navy">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="dist/img/logo.jpg" alt="Logo" width="230" height="120" >
+      <img src="dist/img/logo.jpg" alt="Logo" width="90" height="90" style="-moz-border-radius : 75px;
+  -webkit-border-radius : 75px;
+  border-radius : 75px; margin-left:30%;" >
       
     </a>
 
