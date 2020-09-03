5cb25cb3aa01b63f6ed00bbb2e9851321c807c16
Maissa Soussi <maissa.soussi1920@yahoo.com>
2020-09-03 17:58:48 +0100

    liste des reservations

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app1.css') }}">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @include("layouts.styles")
 
   <!-- Google Font: Source Sans Pro -->
--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -19,9 +19,97 @@
 
     <!-- Main content -->
     <section class="content">
-      
+    <div style="width:250px;">
+    <input class="form-control" id="myInput" type="text" placeholder="Search..">
+    </div>
+    <br>
+    <table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">Ref. Reservation</th>
+      <th scope="col">Cin Client</th>
+      <th scope="col">Imma Véhicule</th>
+      <th scope="col">Date début</th>
+      <th scope="col">Date fin</th>
+      <th scope="col">Montant</th>
+      <th scope="col">#</th>
+    </tr>
+  </thead>
+  <tbody id="myTable">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td><a href="#" class="btn btn-primary">
+         <i class="fas fa-pencil-alt" ></i>
+       </a>
+       <form method="POST" action="#">
+        @csrf
+        @method('DELETE')
+           <button type="submit" class="btn btn-danger">
+             <i class="fa fa-trash"></i>
+           </button>
+       </form>
+       </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td><a href="#" class="btn btn-primary">
+         <i class="fas fa-pencil-alt" ></i>
+       </a>
+       <form method="POST" action="#">
+        @csrf
+        @method('DELETE')
+           <button type="submit" class="btn btn-danger">
+             <i class="fa fa-trash"></i>
+           </button>
+       </form>
+       </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td><a href="#" class="btn btn-primary">
+         <i class="fas fa-pencil-alt" ></i>
+       </a>
+       <form method="POST" action="#">
+        @csrf
+        @method('DELETE')
+           <button type="submit" class="btn btn-danger">
+             <i class="fa fa-trash"></i>
+           </button>
+       </form>
+       </td>
+    </tr>
+  </tbody>
+</table>
     
     </section>
     <!-- /.content -->
   </div>
+
+
+  <script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+
 @endsection
