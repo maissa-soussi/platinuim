5a0f176c95ac93f61526ad3b540f3a6121da0fcc
Maissa Soussi <maissa.soussi1920@yahoo.com>
2020-09-05 18:33:02 +0100

    interfaces update style

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -9,9 +9,9 @@
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
+         <!-- <div class="col-sm-6">
             <h1 class="m-0 text-dark">Clients</h1>
-          </div><!-- /.col -->
+          </div> -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
